fc431bb62f3040471a105db05c8adaed62a44022
Ryo Wakatabe <ryowakatabe@gmail.com>
2013-04-24 15:30:19 +0900

    deleted serial mlm

--- serial_mlm/sim_library/hardware_dependent/wheel/wheel.h
+++
@@ -1,9 +0,0 @@
-#ifndef _WHEEL_H_
-#define _WHEEL_H_
-
-extern hal_motor link_as_right_motor(void);
-extern hal_motor link_as_left_motor(void);
-extern hal_encoder link_as_left_encoder(void);
-extern hal_encoder link_as_right_encoder(void);
-
-#endif //_WHEEL_H_
