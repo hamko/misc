a9f6ce9cde43f752ab7616026027593d63630335
hamko <wakataberyo@gmail.com>
2015-01-02 00:26:35 +0900

    added CoP Marker

--- opt_reg/param.m
+++ opt_reg/param.m
@@ -1,35 +1,41 @@
-global m g M l B C J k K ce; 
+global m g M l B C J k K Kr Ux ce; 
 
 # Controller Parameter
+#### E: -0.77, -2.4
+#Q = diag([1e2,1,1e2,1]); # x xdot theta thetadot
+#R = 1.0;
+#### E: -2.0, -3.1
 Q = diag([1e2,1,1e2,1]); # x xdot theta thetadot
-R = 1.0;
+R = 0.01;
 
 # Control Enable
 ce = 1;
 
 # Simulator 
-n = 100; # Run Time
+n = 300; # Run Time
 initial_theta = 0;
 #initial_theta = pi*3/4;
 
 # Machine Parameter
 m=3;              # [kg]
 M=6;              # [kg]
-l=1.6;            # CoP [m] (NOTE: This will also be identified by experiment)
-J=m*(l**2)/3;     # inertia moment [kg m^2] 
 
+# CoP : NOTE THAT L IS NOT THE LENGTH OF PENDIUM [m] 
+# This is identified by turning the robot upside down and giving micro vibration
+l=0.7;
 # Visco between machine and stick [kg m^2/s] (0.2-2?)
-# This is idenified by turning the robot upside down and giving micro vibration
+# This is identified by turning the robot upside down and giving micro vibration
 C=0.1;
 # motor torque when voltage is maximum and vel is 0. [N]
 # This is identified by running with support and calc. (m+M)a_max_v_0 = (m+M)a_v_0*max_v/v 
 # where a_max_v_0 is acceleration when applying duty = 1.0 and vel = 0.
-k=100;
+k=30;
 # Visco between machine and ground which is for motor force control [kg/s] (0.1-2?)
 # This is identified by running with support and calc. (m+M)a_max_v_0/Vf
 # where Vf is velocity when applying duty = 1.0 and t = inf.
 B=0.1;
 
+J=m*(l**2)/3;     # inertia moment [kg m^2] 
 
 # World Parameter
 g=9.8066;
@@ -47,3 +53,12 @@ g=9.8066;
 
 #### Calc. Controller Parameter
     [K, P, E] = lqr(A, b, Q, R);
+    K
+    E
+
+# Parameter to implement (transfer mat: sensor data->SI unit, ex. robot encoder unit is 1mm, fill unit = [0.001 x x x])
+# gDuty = -(below[1] * gAngle + below[2] * gOmega ...)
+    Uu = diag([k]); # duty 1.0 is k
+    Ux = diag([1 1 1 1]); # 'x' of the robot is as below
+    Kr = inv(Uu)*K*Ux 
+
--- opt_reg/run.m
+++ opt_reg/run.m
@@ -1,12 +1,26 @@
 param; # load machine parameter
 
 function y = u(x, t)
-    global k K;
-    r=3.0; 
-#    r=t;
-    y = -K*[x(1)-r; x(2); x(3); x(4)];
-    duty = y / k;
-    printf("%.2f %.2f %.2f %.2f %.2f %.2f %.2f\n", y, duty, x(2), -(x(1)-r)*K(1), -x(2)*K(2), -x(3)*K(3), -x(4)*K(4));
+    global k K Kr Ux;
+    # Objective Point
+    r=[0; 0; 0; 0]; 
+#    r(1)=3
+    if (t < 0.06*100)
+        r(1)=3;
+    elseif (t < 0.06*200)
+        r(1)=0;
+    elseif (t < 0.06*300)
+        r(1)=-3;
+    endif
+
+    # calc. feedback force
+    y = -K*(x-r);
+    # NOTE: Imitating robot controller, the following line can copy to REAL ROBOT!
+    # in real the later part will be raw data
+    duty = -Kr * (inv(Ux)*x-inv(Ux)*r); 
+
+#    printf("%.2f %.2f %.2f %.2f %.2f %.2f\n", y, duty, -(x(1)-r)*K(1), -x(2)*K(2), -x(3)*K(3), -x(4)*K(4));
+    printf("%.2f %.2f\n", y, duty);
 endfunction
 
 # x xdot theta thetadot
@@ -28,7 +42,7 @@ endfunction
 
 
 #Simulator
-    t = linspace(0, 0.06*n, n)
+    t = linspace(0, 0.06*n, n);
     x0 = [0; 0; initial_theta; 0];
     xx1 = lsode("eom", x0, t);
 
@@ -36,12 +50,15 @@ endfunction
     for i=1:n
         x1 = xx1(i,1); y1 = 0;
         x2 = x1 + l*sin(xx1(i,3)); y2 = l*cos(xx1(i,3));
+        x3 = x1 + 2*l*sin(xx1(i,3)); y3 = 2*l*cos(xx1(i,3));
 
         title(sprintf("x1=%.1f, th1=%.1f, x2=%.1f, th2=%.1f", xx1(i,1),xx1(i,3)));
         hold off; plot([-5.5,5.5],[5.5,5.5]);
         hold on; plot([-5.5,5.5],[-0.1,-0.1]);
-        hold on; plot([-5.5,5.5],[l+0.1,l+0.1]);
+        hold on; plot([-5.5,5.5],[l*2+0.1,l*2+0.1]);
         hold on; plot([x1,x2],[y1,y2]);
+        hold on; scatter(x2, y2, 0.1);
+        hold on; plot([x2,x3],[y2,y3], "r");
 
         pause(0.04);
     endfor
