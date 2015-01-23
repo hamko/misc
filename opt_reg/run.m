0f19e023a71b9fbbbe7c052acf85d9da2e077663
hamko <wakataberyo@gmail.com>
2015-01-23 16:05:22 +0900

    param uptdate

--- opt_reg/run.m
+++ opt_reg/run.m
@@ -1,27 +1,36 @@
 param; # load machine parameter
 
+global flag;
+flag = 0;
 function y = u(x, t)
-    global k K Kv Kr Ux is_pos_control is_easy_vel_control;
+    global k K Kv Kr Uu Ux is_pos_control is_easy_vel_control flag;
 
     # Position Controller
     if (is_pos_control == 1) 
-        r=[3; 0; 0; 0]; # Objective Point
+        r=[1; 0; 0; 0]; # Objective Point
         if (is_easy_vel_control) K(1)=0; endif  # for easy Velocity Control
         y = -K*(x-r); # calc. feedback force
-        duty = -Kr * (inv(Ux)*x-inv(Ux)*r); # in real the later part will be raw data
-        printf("%.2f %.2f %.2f %.2f %.2f\n", y, -(x(1)-r(1))*K(1), -x(2)*K(2), -x(3)*K(3), -x(4)*K(4));
+        duty = -inv(Uu)*K*Ux * (inv(Ux)*x-inv(Ux)*r); # in real the later part will be raw data
+        printf("%.2f %.2f %.2f %.2f %.2f %.2f\n", t, y, -(x(1)-r(1))*K(1), -x(2)*K(2), -x(3)*K(3), -x(4)*K(4));
     else
         # Velocity Controller
         xv = x(2:4);
-        rv=[1; 0; 0];
+        if (t > 3.0)
+            flag = 1;
+        endif 
+        if (!flag)
+            rv=[1; 0; 0];
+        else 
+            rv=[0.0; 0; 0];
+        endif
         y = -Kv*(xv-rv);
-        printf("%.2f %.2f %.2f %.2f\n", y, -(xv(1)-rv(1))*Kv(1), -xv(2)*Kv(2), -xv(3)*Kv(3));
+        printf("%.2f %.2f %.2f %.2f %.2f %.2f %.2f\n", t, xv(1), rv(1), y, -(xv(1)-rv(1))*Kv(1), -xv(2)*Kv(2), -xv(3)*Kv(3));
     endif
 endfunction
 
 # x xdot theta thetadot
 function dx = eom(x, t)
-    global m g M l B C J ce;
+    global m g M l B C J ce count;
 
     # calc. accelation
     if (ce) u = u(x,t); else u = 0; endif 
@@ -52,8 +61,12 @@ endfunction
         hold off; plot([-5.5,5.5],[5.5,5.5]);
         hold on; plot([-5.5,5.5],[-0.1,-0.1]);
         hold on; plot([-5.5,5.5],[l*2+0.1,l*2+0.1]);
+        
         hold on; plot([x1,x2],[y1,y2]);
-        hold on; scatter(x2, y2, 0.1);
         hold on; plot([x2,x3],[y2,y3], "r");
+        hold on; scatter(x2, y2, 0.1);
+
+        hold on; scatter(-5.5, t(i), 0.1);
+
         pause(0.01);
     endfor
