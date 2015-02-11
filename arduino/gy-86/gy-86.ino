e71e60021329ec7d99da58b89db75a286c5bf49f
hamko <wakataberyo@gmail.com>
2015-02-11 09:07:05 +0900

    added manifold

--- arduino/gy-86/gy-86.ino
+++ arduino/gy-86/gy-86.ino
@@ -14,8 +14,8 @@ int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 
 // initial parameter
 float th = 0.0;     // [rad]
-float bi = 1.00;    // [rad/s]
-float sxx = 0.5;    // [rad^2], initial variance of theta (about x3)
+float bi = 0.0;    // [rad/s]
+float sxx = 1;    // [rad^2], initial variance of theta (about x3)
 float sxy = 0;      // [rad rad/s]
 float syy = 0.1;    // [(rad/s)^2], initial variance of bias
 // tuning parameter
@@ -23,20 +23,7 @@ float syy = 0.1;    // [(rad/s)^2], initial variance of bias
 #define VAR_OMEGA 0.1 // [rad/s], variance of omega at 1 rad/s 
 float q = VAR_ACCEL*VAR_ACCEL;      // [(m/s)^2]
 float alpha = VAR_OMEGA*VAR_OMEGA;  // [(rad/s)^2]
-// model parameter
-#ifdef PRECISE_MODE
-#define K_XDD_XD -7.33
-#define K_XDD_TH -16.05
-#define K_XDD_TD 0.464
-#define K_XDD_FC 0.406
-#define K_TDD_XD 9.88
-#define K_TDD_TH 34.88
-#define K_TDD_TD -1.01
-#define K_TDD_FC -0.547
-#define MOMENT_ARM 0.05 // [m]
-#else
 #define FORCE_NOISE 0.2 // [(m/ss)/N], 1N出すことによる加速度センサに乗るだろう標準偏差
-#endif
 
 void kalman(float o, float a, float f)
 {
@@ -46,20 +33,9 @@ void kalman(float o, float a, float f)
     th = p_th + (o-p_bi)*dt;
     sxy = p_sxy - p_syy*dt;
     sxx = p_sxx + (alpha*o*o+p_syy)*dt*dt-2*p_sxy*dt;
-    printf("%f #th\n", th);
-
-    // acce
-#ifdef PRECISE_MODE
-    float xdd = K_XDD_XD*xd+K_XDD_TH*th+K_XDD_TD*o+K_XDD_FC*f;
-    float tdd = K_TDD_XD*xd+K_TDD_TH*th+K_TDD_TD*o+K_TDD_FC*f;
-    a -= (xdd + MOMENT_ARM * tdd);
-#else
-    q += FORCE_NOISE*f*f; // 動いている時は加速度センサの信用をなくす
-#endif
 
     p_th=th, p_bi=bi, p_sxx=sxx, p_syy=syy, p_sxy=sxy;
-    float k1 = g/(g*g*p_sxx+q), k2 = k1*g;
-    //    printf("%f %f %f #z\n", a-g*p_th, a, g*p_th);
+    float k1 = g/(g*g*p_sxx+(q+FORCE_NOISE*f*f)), k2 = k1*g;
     th = p_th + k1*(a-g*sin(p_th))*p_sxx;
     bi = p_bi + k1*(a-g*sin(p_th))*p_sxy;
     sxx = p_sxx - k2*p_sxx*p_sxx;
@@ -83,22 +59,17 @@ void control() {
     GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
     GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
-
     float o = GyX * UTHETAD;
     float a = AcZ * UGRAVITY;
     float f = 0;
 
-    if (t < 400) {bi += o;}
-    else if (t == 400) {bi /= 400; Serial.println(bi,0);}
-    else {
-        //    th += (o - bi)*dt;
-        //    if (t % 100 == 0){Serial.print(o*10000); Serial.print(bi*10000); Serial.print("\n");}
-        //    if (t % 100 == 0){Serial.print(th); Serial.print("\n");}
-        //    if (t % 100 == 0){Serial.print(a); Serial.print("\n");}
-        kalman(o, a, f);
-        if (t % 200 == 0){Serial.print(th); Serial.print("\n");}
-        if {t % 200 == 0}{Serial.print(bi); Serial.print("\n");}
-    }
+    //if (t % 100 == 0){Serial.print(o*10000); Serial.print("\n");Serial.print(bi*10000); Serial.print("\n");}
+    //if (t % 100 == 0){Serial.print(a); Serial.print("\n"); Serial.print(o*10000); Serial.print("\n");}
+    if (t % 200 == 0){Serial.print(th); Serial.print("\n");Serial.print(bi*10000); Serial.print("\n");}
+
+    //th += (o - bi)*dt;
+    kalman(o, a, f);
+
     t++;
 }
 
@@ -109,6 +80,7 @@ void setup(){
     Wire.write((uint8_t)0);     // set to zero (wakes up the MPU-6050)
     Wire.endTransmission();
     Serial.begin(9600);
+    Serial.print("Start.\n");
 }
 
 void loop(){
