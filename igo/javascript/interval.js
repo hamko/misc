d5a8db0e1c0a3b88a9d8c557e65838d24dfd9078
hamko <!!!your>
2015-11-18 00:54:05 +0900

    del igo

--- igo/javascript/interval.js
+++
@@ -1,9 +0,0 @@
-var fps = 100;
-var charsperword = 4.5;
-
-$(function() {
-    $('#speed').change(function() {
-        fps = 1000 / (parseInt($(this).val()) / charsperword) * 60; // 最後の6は1語6文字という意味
-        console.log(fps);
-    });
-});
--- igo/javascript/ruler.js
+++
@@ -1,6 +0,0 @@
-function strWidth(str) {
-    var e = $("#ruler");
-    var width = e.text(str).get(0).offsetWidth;
-    e.empty();
-    return width;
-}
