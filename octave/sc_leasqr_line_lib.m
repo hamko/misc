58fecea91f9187c086a27b3d68a97de8763f5d24
wakatabe <wakataberyo@gmail.com>
2015-11-18 01:34:56 +0900

    lib

--- octave/sc_leasqr_line_lib.m
+++ octave/sc_leasqr_line_lib.m
@@ -1,7 +1,7 @@
 x=[1:50];
 y=rand(1,50).*x;
-[a_u, b_u] = leasqr_line(x, y, 1);
-[a_l, b_l] = leasqr_line(x, y, 0);
+[a_u, b_u] = leasqr_line_lib(x, y, 1);
+[a_l, b_l] = leasqr_line_lib(x, y, 0);
 
 plot(x, y); hold on;
 plot(x, a_u*x+b_u);
