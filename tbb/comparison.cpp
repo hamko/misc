818b88a4ad6a91ab6d733e7f9b6d0fd29714c9b3
Ryo Wakatabe <wakataberyo@gmail.com>
2016-02-17 10:57:13 +0900

    fixed tbb

--- tbb/comparison.cpp
+++ tbb/comparison.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <omp.h>
 #include <cmath>
 #include <cstdlib>
 #include "tbb/task_scheduler_init.h"
@@ -10,60 +11,62 @@
 using namespace std;
 using namespace tbb;
 
-const int x = 100;
-const int rep = 1000000;
+const int task_num = 100;
+const int rep = 10000000;
 
 class Parallel
 {
     private:
-        float *v;
+        int *v;
 
     public:
         void operator() (const blocked_range<int>& range) const 
         {
-            // 0～7までの値を入れる
-            cout << range.begin() << " " << range.end() << endl;
             for (int i = range.begin(); i < range.end(); i++) {
-                int sum = 0; for (int j = 0; j < rep; j++) { sum += rand(); }
-                v[i] = sin(sum % 8);
+                long long sum = 0; for (int j = 0; j < rep; j++) { sum += j; }
+                cout << sum << " ";
             }
         }
 
-        Parallel(float buf[x]) : v(buf) {};
+        Parallel(int buf[task_num]) : v(buf) {};
 };
 
 
 int main()
 {
-    srand((unsigned)time(NULL));
-    float ccc[x];
+    int ccc[task_num];
 
     tick_count start, finish;
 
     // TBB
     start = tick_count::now();
     task_scheduler_init init;
-    parallel_for( blocked_range<int>(0, x), Parallel(ccc), tbb::auto_partitioner());
+    parallel_for( blocked_range<int>(0, task_num), Parallel(ccc), tbb::auto_partitioner());
+    cout << endl;
     init.terminate();
     finish = tick_count::now();
     cout << "tbb=" << (finish - start).seconds() << endl;
 
     // Serial
     start = tick_count::now();
-    for (int i = 0; i < x; i++) {
-        int sum = 0; for (int j = 0; j < rep; j++) { sum += rand(); }
-        ccc[i] = sin(sum % 8);
+    for (int i = 0; i < task_num; i++) {
+        long long sum = 0; for (int j = 0; j < rep; j++) { sum += j; }
+        cout << sum << " ";
     }
+    cout << endl;
     finish = tick_count::now();
     cout << "serial=" << (finish - start).seconds() << endl;
 
     // OpenMP
     start = tick_count::now();
-#pragma omp parallel for
-    for (int i = 0; i < x; i++) {
-        int sum = 0; for (int j = 0; j < rep; j++) { sum += rand(); }
-        ccc[i] = sin(sum % 8);
+    long long sum;
+    int j;
+#pragma omp parallel for private(sum, j)
+    for (int i = 0; i < task_num; i++) {
+        sum = 0; for (j = 0; j < rep; j++) { sum += j; }
+        cout << sum << " ";
     }
+    cout << endl;
     finish = tick_count::now();
     cout << "openmp=" << (finish - start).seconds() << endl;
 
