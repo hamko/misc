a5433105f517fb35bef766b8b7eb2939329025aa
hamko <wakataberyo@gmail.com>
2016-04-13 10:53:45 +0200

    added comments

--- c++/cast/basic_cast.cpp
+++ c++/cast/basic_cast.cpp
@@ -1,3 +1,6 @@
+// 演算子の優先順位は、キャスト、*、+の順番。
+// 異なる型同士の二項演算子は、精度の低い方を暗黙に精度が高い方にキャストする。
+
 #include <bits/stdc++.h> 
 using namespace std; 
 int main(void) 
@@ -6,9 +9,9 @@ int main(void)
     cin >> n;
 
     long long a;
-    a = n * n * n; cout << a << endl;
-    a = (long long)n * n * n; cout << a << endl;
-    a = (long long)n * n * n - n * n * n; cout << a << endl;
-    a = (long long)n * n * n - (long long)n * n * n; cout << a << endl;
+    a/*NG*/ = n * n * n; cout << a << endl;
+    a/*OK*/ = (long long)n * n * n; cout << a << endl;
+    a/*NG*/ = (long long)n * n * n - n * n * n; cout << a << endl;
+    a/*OK*/ = (long long)n * n * n - (long long)n * n * n; cout << a << endl;
     return 0;
 }
